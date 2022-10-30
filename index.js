55a3425d1ee8e2a84c0fcba6c0951d7ecec61974
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-10-30 22:27:32 +0100

    added command parser

--- index.js
+++ index.js
@@ -29,9 +29,63 @@ const question = [
     }
 ]
 
+const commands = [
+    {
+        name: 'players',
+        message: 'Players online: ',
+        explanation: 'Shows the players online',
+        function: () => {
+            console.table(Object.values(bot.players).map(player => ({
+                username: player.username,
+                ping: player.ping,
+                uuid: player.uuid,
+                gamemode: player.gamemode,
+                position: player.entity.position.x + ', ' + player.entity.position.y + ', ' + player.entity.position.z
+            })))
+        }
+    },
+    {
+        name: 'stop',
+        message: 'Stopping bot...',
+        explanation: 'Stops the bot',
+        function: () => {
+            bot.end()
+        }
+    },
+    {
+        name: 'help',
+        message: '',
+        explanation: 'Shows this help menu',
+        function: () => {
+            console.log("Start a message with '-' to use a command");
+            const commandArray = commands.map(command => {
+                return {
+                    command: command.name,
+                    explanation: command.explanation
+                }
+            })
+            console.table(commandArray)
+        }
+    }
+]
+
 function chat() {
     inquirer.prompt(question).then(answers => {
-        bot.chat(answers.message)
-        chat()
+        if (answers.message.startsWith('-')) {
+            const command = answers.message.split(' ')[0].slice(1)
+            const commandObject = commands.find(c => c.name === command)
+
+            if (commandObject) {
+                console.log(commandObject.message);
+                commandObject.function()
+                chat()
+            } else {
+                console.log('MINESTRESSER: Invalid command. Run -help for a list of commands')
+                chat()
+            }
+        } else {
+            bot.chat(answers.message)
+            chat()
+        }
     })
 }
