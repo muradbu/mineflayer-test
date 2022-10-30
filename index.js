b73a25dd4c076b9f49e2e2da8d5bc92bd8630fb0
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-10-30 18:53:42 +0100

    added chat functionality

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 require('dotenv').config()
 const mineflayer = require('mineflayer')
+const prompt = require('prompt')
 
-mineflayer.createBot({
+const bot = mineflayer.createBot({
     host: process.env.HOST,
     port: process.env.PORT || 25565,
     username: process.env.USERNAME,
-    password: process.env.PASSWORD
+    password: process.env.PASSWORD,
+    auth: 'microsoft'
+})
+
+bot.on('chat', (username, message) => {
+    console.log(`${username}: ${message}`);
+})
+
+bot.on('login', () => {
+    prompt.start()
+    prompt.get('message', (err, result) => {
+        bot.chat(result.message)
+
+        if (err) {
+            console.log("ERROR: ", err)
+        }
+    })
 })
