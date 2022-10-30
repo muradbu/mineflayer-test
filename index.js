55d37990312f3c8b1d13a525d96ffc1bab47d6d7
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-10-30 18:19:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const mineflayer = require('mineflayer')
+
+mineflayer.createBot({
+    host: process.env.HOST,
+    port: process.env.PORT || 25565,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+})
